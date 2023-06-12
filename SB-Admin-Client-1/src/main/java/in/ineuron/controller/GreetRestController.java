a4d8803b33a10d1f1b19580b7892beb73e0fbdde
Suresh <senchasuresh99@gmail.com>
2023-06-12 15:41:39 +0530

    commit by suresh sencha

---
+++ SB-Admin-Client-1/src/main/java/in/ineuron/controller/GreetRestController.java
@@ -0,0 +1,34 @@
+package in.ineuron.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/greet")
+public class GreetRestController {
+	
+	@GetMapping("/wish/{name}")
+	public ResponseEntity<String> sayHello(@PathVariable String name) {
+		LocalDateTime ldt = LocalDateTime.now();
+		int hour = ldt.getHour();
+
+		String body = null;
+
+		if (hour < 12) {
+			body = "Good Morning :: " + name;
+		} else if (hour < 16) {
+			body = "Good Afternoon :: " + name;
+		} else if (hour < 20) {
+			body = "Good Evening :: " + name;
+		} else {
+			body = "Good Night :: " + name;
+		}
+		return new ResponseEntity<String>(body, HttpStatus.OK);
+	}
+}
